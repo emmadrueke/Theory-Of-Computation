286f05fd21b169f2a649a58d380f085be4891887
Emma Drueke <emma.drueke@gmaila.com>
2018-03-22 09:34:12 -0500

    First Commit

--- projects/state-mach-regex/phone/phone.js
+++ projects/state-mach-regex/phone/phone.js
@@ -1,4 +1,7 @@
 var readline = require('readline');
+const fs = require('fs');
+
+const regex = /\(?(\d{3})\)?\s*-?\s*(\d{3})\s*-?\s*(\d{4})/;
 
 var rl = readline.createInterface({
   input: process.stdin,
@@ -7,17 +10,15 @@ var rl = readline.createInterface({
 });
 
 
+
 // This code reads a line at a time from stdin
 
 rl.on('line', function (line) {
+  const matches = line.match(regex);
 
-    // !!!! IMPLEMENT ME
-
-    // Come up with the phone regex
-
-    // Find matches
-
-    // If match found, print number with no spaces, parens, or dashes
-
-    // Else print that no number was found
+  if (matches === null) {
+      console.log("No phone number found");
+  } else {
+      console.log(`${matches[1]}${matches[2]}${matches[3]}`);
+  }
 });
